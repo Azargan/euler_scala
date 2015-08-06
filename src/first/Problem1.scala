7008c36061687d9c03aacc5120f136fd426dd093
Aliaksei Vihuro <aliaksei_vihuro@epam.com>
2015-08-06 21:59:48 +0300

    First problem was solved

---
+++ src/first/Problem1.scala
@@ -0,0 +1,30 @@
+package first
+
+import scala.annotation.tailrec
+
+/**
+ * Created by azargan on 06.08.15.
+ */
+/*
+ * Problem description:
+ *
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5,
+ * we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ *
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+object Problem1 {
+
+  def multipleForNumber(number: Int) : Int = {
+
+    (1 until number).filter(i => i % 3 == 0 || i % 5 == 0).sum
+  }
+
+  def main(args: Array[String]) {
+    println(multipleForNumber(1000))
+  }
+
+}
+/*
+ * Solved[06/08/15]: 233168
+ */
